f34dd3867817e0bed798bbb5163384bd5d23a665
NewCesam <ewen.madec@outlook.fr>
2026-04-27 17:25:13 +0100

    Init Spring Boot project

---
+++ README.md
@@ -0,0 +1,3 @@
+# Medisync - Backend
+
+Backend de l'application Web et Mobile Medisync, développée dans le cadre de l'enseignement Développement Web et Mobile délivré par M. Toulni.
---
+++ src/test/java/ma/medisync/medisync_backend/MedisyncBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.medisync.medisync_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedisyncBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
